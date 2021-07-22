65899e405ec8f0b42e2a890433274ef9e0b206f6
Taher <ttaherhagui@gmail.com>
2021-07-22 20:19:50 +0100

    my first push to production.

---
+++ java/script.js
@@ -0,0 +1,22 @@
+
+
+/*i=0;
+j=0;
+function titleAnimation(){
+      
+      title = "Taher Haggui" ; 
+      position = "Data Scientist";
+
+      t=setTimeout(titleAnimation,Math.max(1000-i*300,500));
+      i++;
+      j++;
+      if ((i <= title.length) || (j<=position.length)){
+         document.getElementById("mon_nom").innerHTML= title.substring(0,i);
+         document.querySelector("h2").innerHTML = position.substring(0,j);
+      }
+
+      else 
+         clearTimeout(t);
+
+      
+}*/
